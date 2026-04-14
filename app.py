9a884e08ff1518b9eed201bd0c555f01536649ca
Jaskeerat-Singh909 <jaskeeratsingh909@gmail.com>
2026-04-14 13:30:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,68 @@
 import streamlit as st
 import pickle
 import requests
-
-def fetch_poster(movie_id):
-    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=ae5d858b83a2085e55c3904041bb9aad&language=en-US"
-    data = requests.get(url)
-    data = data.json()
-    
-    if data['poster_path'] is None:
-        return "https://via.placeholder.com/500x750?text=No+Image"
-    
-    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-
-movies = pickle.load(open('movies.pkl', 'rb'))
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+
+st.markdown("""
+<style>
+.stApp {
+    background-color: #0e1117;
+    color: white;
+}
+
+/* Title */
+.title {
+    text-align: center;
+    color: #E50914;
+    font-size: 50px;
+    font-weight: bold;
+}
+
+/* Subtitle */
+.subtitle {
+    text-align: center;
+    font-size: 18px;
+    color: #bbbbbb;
+}
+
+/* Button */
+.stButton>button {
+    background-color: #E50914;
+    color: white;
+    border-radius: 10px;
+    height: 3em;
+    width: 100%;
+    font-size: 18px;
+    font-weight: bold;
+}
+
+/* Movie card */
+.movie-card {
+    background-color: #181818;
+    padding: 10px;
+    border-radius: 10px;
+    text-align: center;
+}
+</style>
+""", unsafe_allow_html=True)
+
+movies = pickle.load(open('movies.pkl', 'rb'))
+
 tfidf = TfidfVectorizer(max_features=5000, stop_words='english')
 vectors = tfidf.fit_transform(movies['tags']).toarray()
-
 similarity = cosine_similarity(vectors)
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=ae5d858b83a2085e55c3904041bb9aad&language=en-US"
+    data = requests.get(url).json()
+
+    if data.get('poster_path') is None:
+        return "https://via.placeholder.com/500x750?text=No+Image"
+
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
     if movie not in movies['title'].values:
@@ -28,49 +71,51 @@ def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
 
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)),
+                         reverse=True,
+                         key=lambda x: x[1])[1:6]
 
-    recommended_movies = []
-    recommended_posters = []
+    names = []
+    posters = []
 
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-        
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_posters.append(fetch_poster(movie_id))
+        names.append(movies.iloc[i[0]].title)
+        posters.append(fetch_poster(movie_id))
 
-    return recommended_movies, recommended_posters
+    return names, posters
 
-st.title("🎬 Movie Recommender System")
+st.markdown('<div class="title">🎬 Movie Recommender</div>', unsafe_allow_html=True)
+st.markdown('<div class="subtitle">Find movies you’ll love instantly 🍿</div>', unsafe_allow_html=True)
 
-selected_movie = st.selectbox(
-    "Select a movie",
-    movies['title'].values
-)
-if st.button('Recommend'):
-    names, posters = recommend(selected_movie)
+st.write("")
 
-    if len(names) == 0:
-        st.write("Movie not found!")
-    else:
-        col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3 = st.columns([1,2,1])
+with col2:
+    selected_movie = st.selectbox("Search Movie", movies['title'].values)
 
-        with col1:
-            st.text(names[0])
-            st.image(posters[0])
+st.write("")
 
-        with col2:
-            st.text(names[1])
-            st.image(posters[1])
+col1, col2, col3 = st.columns([1,1,1])
+with col2:
+    clicked = st.button("🎯 Recommend")
 
-        with col3:
-            st.text(names[2])
-            st.image(posters[2])
+if clicked:
+    with st.spinner("Fetching recommendations... 🍿"):
+        names, posters = recommend(selected_movie)
 
-        with col4:
-            st.text(names[3])
-            st.image(posters[3])
+        if len(names) == 0:
+            st.warning("Movie not found!")
+        else:
+            st.markdown("---")
+            st.subheader("✨ Recommended for you")
 
-        with col5:
-            st.text(names[4])
-            st.image(posters[4])
+            cols = st.columns(5)
+
+            for i in range(5):
+                with cols[i]:
+                    st.image(posters[i])
+                    st.markdown(
+                        f"<div class='movie-card'><b>{names[i]}</b></div>",
+                        unsafe_allow_html=True
+                    )
